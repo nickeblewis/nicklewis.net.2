6e1bfe87f720db0c70ac3baa4a694a13085b4e3f
Nick Lewis <nickeblewis@gmail.com>
2018-03-04 16:13:16 +0000

    first commit

---
+++ config/SiteConfig.js
@@ -0,0 +1,26 @@
+module.exports = {
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
+
+  siteTitle: 'Nick Lewis', // Navigation and Site Title
+  siteTitleAlt: 'Nick Lewis - Developer etc', // Alternative Site title for SEO
+  siteUrl: 'https://minimal-blog.netlify.com', // Domain of your site. No trailing slash!
+  siteLanguage: 'en', // Language Tag on <html> element
+  siteLogo: '/logos/logo-1024.png', // Used for SEO and manifest
+  siteBanner: '/social/banner.jpg',
+  siteDescription: 'Minimal Blog with big typography',
+
+  siteFBAppID: '123456789', // Facebook App ID
+  googleAnalyticsID: "UA-159000-18", // GA tracking ID.
+  userTwitter: 'minimal', // Twitter Username
+  ogSiteName: 'minimal', // Facebook Site Name
+
+  // Manifest and Progress color
+  themeColor: '#3498DB',
+  backgroundColor: '#2b2e3c',
+
+  // Settings for typography.js
+  headerFontFamily: 'Bitter',
+  bodyFontFamily: 'Open Sans',
+  baseFontSize: '18px',
+};
+
